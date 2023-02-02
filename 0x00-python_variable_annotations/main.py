580cae284605fe705cea6d51108020e1939a2611
AturiheihiBledon <2020blendon@gmail.com>
2023-02-02 18:03:55 +0300

    Task 7

--- 0x00-python_variable_annotations/main.py
+++ 0x00-python_variable_annotations/main.py
@@ -55,10 +55,17 @@
 # print("sum_list(floats) returns {} which is a {}".format(floats_sum, type(floats_sum)))
 
 # Task 6 case
-sum_mixed_list = __import__('6-sum_mixed_list').sum_mixed_list
+# sum_mixed_list = __import__('6-sum_mixed_list').sum_mixed_list
 
-print(sum_mixed_list.__annotations__)
-mixed = [5, 4, 3.14, 666, 0.99]
-ans = sum_mixed_list(mixed)
-print(ans == sum(mixed))
-print("sum_mixed_list(mixed) returns {} which is a {}".format(ans, type(ans)))
+# print(sum_mixed_list.__annotations__)
+# mixed = [5, 4, 3.14, 666, 0.99]
+# ans = sum_mixed_list(mixed)
+# print(ans == sum(mixed))
+# print("sum_mixed_list(mixed) returns {} which is a {}".format(ans, type(ans)))
+
+# Task 7 case
+to_kv = __import__('7-to_kv').to_kv
+
+print(to_kv.__annotations__)
+print(to_kv("eggs", 3))
+print(to_kv("school", 0.02))
