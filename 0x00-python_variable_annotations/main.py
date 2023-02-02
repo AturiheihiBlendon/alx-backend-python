47f4b82907cb81a0cd410cba2434fad463af5b17
AturiheihiBledon <2020blendon@gmail.com>
2023-02-02 10:22:14 +0300

    Task 1

--- 0x00-python_variable_annotations/main.py
+++ 0x00-python_variable_annotations/main.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
 # Task 0 case
-add = __import__('0-add').add
+# add = __import__('0-add').add
 
-print(add(1.11, 2.22) == 1.11 + 2.22)
-print(add.__annotations__)
+# print(add(1.11, 2.22) == 1.11 + 2.22)
+# print(add.__annotations__)
+
+# Task 1 case
+concat = __import__('1-concat').concat
+
+str1 = "egg"
+str2 = "shell"
+
+print(concat(str1, str2) == "{}{}".format(str1, str2))
+print(concat.__annotations__)
