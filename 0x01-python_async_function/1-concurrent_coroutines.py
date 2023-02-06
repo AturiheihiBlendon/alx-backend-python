40b1b4f0f418d77b051083bb7d329355c617ddb9
AturiheihiBledon <2020blendon@gmail.com>
2023-02-06 13:16:25 +0300

    Task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Concurrent coroutines
+"""
+
+import random
+import asyncio
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    return list of all delays
+    """
+    lst, fArray = [], []
+    for _ in range(n):
+        lst.append(wait_random(max_delay))
+
+    for task in asyncio.as_completed(lst):
+        result = await task
+        fArray.append(result)
+    return fArray
+
+
--- 0x01-python_async_function/test.py
+++ 0x01-python_async_function/test.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python3
 
 # Task 0 testcase
+# import asyncio
+
+# wait_random = __import__('0-basic_async_syntax').wait_random
+
+# print(asyncio.run(wait_random()))
+# print(asyncio.run(wait_random(5)))
+# print(asyncio.run(wait_random(15)))
+
+# Task 1 test case
+#!/usr/bin/env python3
+'''
+Test file for printing the correct output of the wait_n coroutine
+'''
 import asyncio
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
-print(asyncio.run(wait_random()))
-print(asyncio.run(wait_random(5)))
-print(asyncio.run(wait_random(15)))
+print(asyncio.run(wait_n(5, 5)))
+print(asyncio.run(wait_n(10, 7)))
+print(asyncio.run(wait_n(10, 0)))
