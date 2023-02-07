7a98eca1acbee33d02b4351dc7178d1f1901e1f4
AturiheihiBledon <2020blendon@gmail.com>
2023-02-07 13:39:58 +0300

    Fixed type hinting generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,9 +4,13 @@ Async Generator
 """
 import random
 import asyncio
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    yields a random number each time a coroutine is run
+    """
     for _ in range(10):
         yield random.random()
         await asyncio.sleep(1)
