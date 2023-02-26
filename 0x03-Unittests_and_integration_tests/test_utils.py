ee379293e821786eb9d1497416007b83257b61a3
AturiheihiBlendon <2020blendon@gmail.com>
2023-02-26 22:33:19 +0300

    Task 0 [parametize unit test]

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" Unittests for utils.py """
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    ''' class to test access_nested_map function '''
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        ''' test access_nested_map function '''
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        ''' test that exception raises KeyErro'''
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
